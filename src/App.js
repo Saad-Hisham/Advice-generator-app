5860b13d328ae156ade6f4a5d45efc2ca74d6a98
saad hesham <saadhesham626@gmail.com>
2023-09-13 23:58:49 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-
+import axios from "axios";
+import { useState, useCallback, useEffect } from "react";
+import divider from "./advice-generator-app-main/images/pattern-divider-desktop.svg"
+import button from "./advice-generator-app-main/images/icon-dice.svg"
 function App() {
+  const [advice, setAdvice] = useState('');
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+  const fetchAdvice = useCallback(async () => {
+    try {
+      const response = await axios.get('https://api.adviceslip.com/advice');
+      const adviceText = response.data.slip;
+      setAdvice(adviceText);
+    } catch (error) {
+      console.log('Error fetching advice:', error);
+    }
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className="app-container">
+        <div>
+        <div className="id-container">
+        ADVICE #{advice.id}
+        </div>
+       <div className="advice-container">
+       {advice.advice}
+
+       </div>
+      <div className="dvider">
+        <img src={divider} alt="divider"/>
+      </div>
+      <div className="button-container">
+      <button onClick={fetchAdvice}>
+        <img src={button} alt="button"/>
+      </button>
+      </div>
+    </div>
+    </div>
+    <footer>
+        <p class="attribution">
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+          Coded with ❤️ by <a href="https://www.frontendmentor.io/profile/Saad-Hisham">Saad Hisham 😼</a>.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </footer>
     </div>
   );
 }
