19075c91d6ee8195832d0b2b4fbf9de085cdb1f7
Saad Hesham <132556058+Saad-Hisham@users.noreply.github.com>
2023-09-21 12:31:34 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,76 @@
+// import axios from "axios";
+// import { useState, useCallback, useEffect } from "react";
+// import divider from "./advice-generator-app-main/images/pattern-divider-desktop.svg"
+// import button from "./advice-generator-app-main/images/icon-dice.svg"
+// function App() {
+//   const [advice, setAdvice] = useState('');
+//   useEffect(() => {
+//     fetchAdvice();
+//   }, []);
+//   const fetchAdvice = useCallback(async () => {
+//     try {
+//       const response = await axios.get('https://api.adviceslip.com/advice', { 
+//       method: 'GET', 
+//       mode: 'cors', 
+//       cache: 'no-cache' 
+// })
+//       const adviceText = response.data.slip;
+//       setAdvice(adviceText);
+//     } catch (error) {
+//       console.log('Error fetching advice:', error);
+//     }
+//   }, []);
+
+//   return (
+//     <div className="App">
+//       <div className="app-container">
+//         <div>
+//         <div className="id-container">
+//         ADVICE #{advice.id}
+//         </div>
+//        <div className="advice-container">
+//        {advice.advice}
+
+//        </div>
+//       <div className="dvider">
+//         <img src={divider} alt="divider"/>
+//       </div>
+//       <div className="button-container">
+//       <button onClick={fetchAdvice}>
+//         <img src={button} alt="button"/>
+//       </button>
+//       </div>
+//     </div>
+//     </div>
+//     <footer>
+//         <p class="attribution">
+//           Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+//           Coded with ❤️ by <a href="https://www.frontendmentor.io/profile/Saad-Hisham">Saad Hisham 😼</a>.
+//         </p>
+//       </footer>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
 import axios from "axios";
 import { useState, useCallback, useEffect } from "react";
-import divider from "./advice-generator-app-main/images/pattern-divider-desktop.svg"
-import button from "./advice-generator-app-main/images/icon-dice.svg"
+import divider from "./advice-generator-app-main/images/pattern-divider-desktop.svg";
+import button from "./advice-generator-app-main/images/icon-dice.svg";
+
 function App() {
-  const [advice, setAdvice] = useState('');
+  const [advice, setAdvice] = useState(null);
+
   useEffect(() => {
     fetchAdvice();
   }, []);
+
   const fetchAdvice = useCallback(async () => {
     try {
-      const response = await axios.get('https://api.adviceslip.com/advice', { 
-      method: 'GET', 
-      mode: 'cors', 
-      cache: 'no-cache' 
-})
-      const adviceText = response.data.slip;
+      const response = await axios.get('https://api.adviceslip.com/advice');
+      const adviceText = response.data.slip.advice;
       setAdvice(adviceText);
     } catch (error) {
       console.log('Error fetching advice:', error);
@@ -24,27 +80,24 @@ function App() {
   return (
     <div className="App">
       <div className="app-container">
-        <div>
         <div className="id-container">
-        ADVICE #{advice.id}
+          ADVICE #{advice && advice.id}
         </div>
-       <div className="advice-container">
-       {advice.advice}
-
-       </div>
-      <div className="dvider">
-        <img src={divider} alt="divider"/>
+        <div className="advice-container">
+          {advice && advice.advice}
+        </div>
+        <div className="divider">
+          <img src={divider} alt="divider" />
+        </div>
+        <div className="button-container">
+          <button onClick={fetchAdvice}>
+            <img src={button} alt="button" />
+          </button>
+        </div>
       </div>
-      <div className="button-container">
-      <button onClick={fetchAdvice}>
-        <img src={button} alt="button"/>
-      </button>
-      </div>
-    </div>
-    </div>
-    <footer>
-        <p class="attribution">
-          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+      <footer>
+        <p className="attribution">
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noopener noreferrer">Frontend Mentor</a>.
           Coded with ❤️ by <a href="https://www.frontendmentor.io/profile/Saad-Hisham">Saad Hisham 😼</a>.
         </p>
       </footer>
